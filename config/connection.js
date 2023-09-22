63605248f5815bd973410a9a250aeaed7dec8bdd
Jese H <pilers2000@hotmail.com>
2023-09-22 14:33:18 -0400

    added jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,38 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// module.exports = sequelize;
+
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+// Check if the app is running in a production environment (Heroku)
+if (process.env.NODE_ENV === 'production') {
+  // Use the DATABASE_URL environment variable provided by Heroku
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  // Use your local development configuration
+  var sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
